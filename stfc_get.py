8039d7973e324bfd84db0f8cd8cf3ff349b312c7
Al Ferguson <aferguson@neptunescove.org>
2025-12-16 22:15:39 +1100

    Minor refactoring to improve clarity.

--- stfc_get.py
+++ stfc_get.py
@@ -16,45 +16,46 @@ NOTE: This has been tested with Python 3.9+ and Requests v2.32+
 #   - typing
 
 import requests as rq
+
 # endregion Imports
 
 # region Author & Version
 __author__: str = "Al Ferguson"
-__updated__ = "2025-03-06 09:25:02"
+__updated__ = "2025-12-16 22:14:26"
 __version__: str = "0.2.6"
 # endregion Author & Version
 
 # region Functions
 
 
-def get_json_value(
-    json_data: tuple[dict], srchid: str, srchkey: str, dflt: str = ""
-) -> str:
+def get_json_value(json_data: tuple[dict], srchid: str, srchkey: str, dflt: str = "") -> str:
     """Get a value from a JSON Dictionary for the given key"""
     return next(
-        (item["text"]
-         for item in json_data
-         if (item["id"] == srchid and item["key"] == srchkey)
-         ),
-        dflt
-        )
+        (
+            item["text"]
+            for item in json_data
+            if item.get("id") == srchid and item.get("key") == srchkey
+        ),
+        dflt,
+    )
 
 
 def generate_ship_import() -> str:
-    """generate_ship_import Builds the SQL for the STFC Ships Import File"""
-    return SHIP_INSERT + ",\n".join(construct_ship_row(ship) for ship in SHIP)
+    """Build the SQL for the STFC Ships Import File"""
+    ship_rows = [construct_ship_row(ship) for ship in SHIP]
+    return SHIP_INSERT + ",\n".join(ship_rows)
 
 
 def construct_ship_row(ship: dict) -> str:
-    """Constructs a ship row string from the given dictionary."""
-    factid: str = ship.get("faction", {}).get("loca_id", "0")
-    return (
-        f'({ship["id"]}, '
-        f'{construct_shipnames(ship["loca_id"])}, '
-        f'{ship["grade"]}, '
-        f'"{SHIPTYPES[ship["hull_type"]]}", '
-        f'{construct_faction(factid)})'
-    )
+    """Construct a ship row string from the given dictionary."""
+    factid = ship.get("faction", {}).get("loca_id", "0")
+    ship_id = str(ship["id"])
+    ship_name = construct_shipnames(ship["loca_id"])
+    grade = str(ship["grade"])
+    ship_type = SHIPTYPES[ship["hull_type"]]
+    faction = construct_faction(factid)
+
+    return f'({ship_id}, {ship_name}, {grade}, "{ship_type}", {faction})'
 
 
 def construct_shipnames(shipid: str) -> str:
@@ -63,30 +64,29 @@ def construct_shipnames(shipid: str) -> str:
 
 
 def construct_faction(factid: str) -> str:
-    """construct_faction builds Faction Name from factions dictionary"""
+    """Build Faction Name from factions dictionary"""
     return f'"{get_json_value(FACTIONS, factid, "faction_name", "Neutral")}"'
 
 
 def generate_system_import() -> str:
-    """generate_system_import builds SQL for the STFC Systems Import File"""
-    return SYSTEM_INSERT + ",\n".join(construct_system_row(system)
-                                      for system in SYSTEM)
+    """Build SQL for the STFC Systems Import File"""
+    return SYSTEM_INSERT + ",\n".join(construct_system_row(system) for system in SYSTEM)
 
 
 def construct_system_row(system: dict) -> str:
-    """Builds SQL import data line from system dictionary."""
+    """Build SQL import data line from system dictionary."""
     try:
         factid: str = system["hostiles"][0]["faction"]["loca_id"]
     except IndexError:
         factid = "0"
     return (
-        f'({system["id"]}, '
-        f'{construct_system_names(system["id"])}, '
-        f'{system["level"]}, '
-        f'{system["est_warp"]}, '
+        f"({system['id']}, "
+        f"{construct_system_names(system['id'])}, "
+        f"{system['level']}, "
+        f"{system['est_warp']}, "
         f"{construct_faction(factid)}, "
-        f'{system["is_deep_space"]}, '
-        f'{system["is_mirror_universe"]})'
+        f"{system['is_deep_space']}, "
+        f"{system['is_mirror_universe']})"
     )
 
 
@@ -103,8 +103,7 @@ TRANSLATE_LANGUAGE = "en"
 DETAIL_URL: str = f"{API_URL}/translations/{TRANSLATE_LANGUAGE}"
 
 SHIP_INSERT: str = (
-    "INSERT INTO `StfcShips` (`ShipID`, `ShipName`,"
-    "`ShipLevel`, `ShipType`,`ShipFaction`) VALUES\n"
+    "INSERT INTO `StfcShips` (`ShipID`, `ShipName`,`ShipLevel`, `ShipType`,`ShipFaction`) VALUES\n"
 )
 SYSTEM_INSERT: str = (
     "INSERT INTO `StfcSystems` (`SystemID`, `SystemName`, "
@@ -115,18 +114,11 @@ SQL_END = ";\n\nCOMMIT;\n\n"
 
 VERSION: str = rq.get(f"{API_URL}/version.txt", timeout=5).text
 
-SHIP: tuple[dict] = rq.get(f"{API_URL}/ship/summary.json?version={VERSION}",
-                           timeout=5).json()
-SHIPS: tuple[dict] = rq.get(f"{DETAIL_URL}/ships.json?version={VERSION}",
-                            timeout=5).json()
-SYSTEM: tuple[dict] = rq.get(
-    f"{API_URL}/system/summary.json?version={VERSION}",
-    timeout=5
-).json()
-SYSTEMS: tuple[dict] = rq.get(f"{DETAIL_URL}/systems.json?version={VERSION}",
-                              timeout=5).json()
-FACTIONS: tuple[dict] = rq.get(f"{DETAIL_URL}/factions.json?version={VERSION}",
-                               timeout=5).json()
+SHIP: tuple[dict] = rq.get(f"{API_URL}/ship/summary.json?version={VERSION}", timeout=5).json()
+SHIPS: tuple[dict] = rq.get(f"{DETAIL_URL}/ships.json?version={VERSION}", timeout=5).json()
+SYSTEM: tuple[dict] = rq.get(f"{API_URL}/system/summary.json?version={VERSION}", timeout=5).json()
+SYSTEMS: tuple[dict] = rq.get(f"{DETAIL_URL}/systems.json?version={VERSION}", timeout=5).json()
+FACTIONS: tuple[dict] = rq.get(f"{DETAIL_URL}/factions.json?version={VERSION}", timeout=5).json()
 
 SHIPTYPES: tuple = ("Interceptor", "Survey", "Explorer", "Battleship")
 # endregion Global Variables
